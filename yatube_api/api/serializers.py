5578e89a93dbf6d7d3fbf9afd750bbb69f91da9d
Vlad Brusensky <brusen_sky@mail.ru>
2022-06-09 01:25:36 +0300

    09/06 1 отправка

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,66 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.serializers import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
+from posts.models import Comment, Group, Post, Follow
 
-from posts.models import Comment, Post
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
-
-    class Meta:
-        fields = '__all__'
-        model = Post
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+    author = SlugRelatedField(
+        read_only=True,
+        slug_field='username'
     )
 
     class Meta:
-        fields = '__all__'
+        model = Post
+        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('title', 'slug', 'description')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
         model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+        validators = [UniqueTogetherValidator(
+            queryset=Follow.objects.all(), fields=['user', 'following'],
+            message='Вы уже подписаны на этого автора'
+        )]
+
+    def validate(self, data):
+        if (
+                self.context['request'].method == 'POST'
+                and data['user'] == data['following']
+        ):
+            raise serializers.ValidationError(
+                'Нарциссизм это плохо'
+            )
+        return data
